e7eb0a64ad02707b509f8c7471278c42caa2b54a
Viktoria <zaharova181114@gmail.com>
2022-05-18 09:24:57 +0300

    media1

--- app/js/common.js
+++ app/js/common.js
@@ -12,5 +12,58 @@ $('.cases-slider').slick({
     arrows: false,
 });
 
+$('.home-slider').slick({
+    slidesToShow: 1,
+    fade: true,
+    arrows: false,
+    dots: true,
+    appendDots: '.home-slider-dots'
+});
+
+$('.home-slider-navigation').slick({
+    slidesToShow: 4,
+    arrows: false,
+    focusOnSelect: true,
+    asNavFor: '.home-slider',
+    responsive: [
+        {
+            breakpoint: 850,
+            settings: {
+                slidesToShow: 3,
+
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 2,
+                autoplay: true,
+                autoplaySpeed: 3000,
+            }
+        },
+        {
+            breakpoint: 420,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+let homeSlider = $('.home-slider');
+
+$('.counter-slide__default').text("/" + homeSlider.slick("getSlick").slideCount);
+
+homeSlider.on('afterChange', function(event, slick, currentSlide){
+    $(".counter-slide__cp").text(currentSlide<10?`${currentSlide+1}`:currentSlide+1);
+});
+
+// mobile menu
+$('.btn-burger').on('click', function () {
+   $('.mobile-menu').fadeToggle();
+});
+
+
 // mask phone
 $('[name="phone"]').mask('+7(999) 999 - 99 - 99');
